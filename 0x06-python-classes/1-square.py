383f4a801628fbd002fdaf9b64e2eb9d495665d8
roochieng <49204011+roochieng@users.noreply.github.com>
2023-05-30 16:47:10 +0000

    Write a class Square that defines a square by: (based on 0-square.py)

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Defining a square class"""
+
+
+class Square:
+    """ Define a square class """
+    def __init__(self, size):
+        """ Initializing a square class
+        Argument: size: size of the square
+         """
+        self.__size = size
