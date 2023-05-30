9f9524fd2874621308896848be82f4c0d982782a
roochieng <49204011+roochieng@users.noreply.github.com>
2023-05-30 17:50:34 +0000

    Write a class Square that defines a square by: (based on 4-square.py)

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-""" Create a square class"""
+""" Creating a square class """
 
 
 class Square:
-    """ Define a class square """
+    """ Defining a class square """
     def __init__(self, size=0):
         """ Initializing a square class
-        Arguments: size=0: size of the square
+        Args: size=0: size of the square
          """
         self.__size = size
 
     @property
     def size(self):
-        """ Get the size of the square """
+        """ Getting the size of the square """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """ Set the size of the square """
+        """ Setting the size of the square """
         if type(value) is not int:
             raise TypeError("size must be an integer")
         if value < 0:
@@ -25,29 +25,29 @@ class Square:
         self.__size = value
 
     def area(self):
-        """ Calculate the area of the square """
+        """ Calculating the area of the square """
         return (self.__size ** 2)
 
-    def __eql__(self, other):
-        """ Compare two squares """
+    def __eq__(self, other):
+        """ Comparing two squares """
         return self.area() == other.area()
 
-    def __nte__(self, other):
-        """ Compare != two squares """
+    def __ne__(self, other):
+        """ Comparing != two squares """
         return self.area() != other.area()
 
-    def __les__(self, other):
+    def __lt__(self, other):
         """ Comparing < two squares """
         return self.area() < other.area()
 
-    def __lese__(self, other):
-        """ Compare <= two squares """
+    def __le__(self, other):
+        """ Comparing <= two squares """
         return self.area() <= other.area()
 
-    def __grt__(self, other):
-        """ Compare > two squares """
+    def __gt__(self, other):
+        """ Comparing > two squares """
         return self.area() > other.area()
 
-    def __grte__(self, other):
-        """ Compare >= two squares """
+    def __ge__(self, other):
+        """ Comparing >= two squares """
         return self.area() >= other.area()
