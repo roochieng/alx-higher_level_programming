a7a69eeb594bff639f02d362b6875a86336e02c3
roochieng <49204011+roochieng@users.noreply.github.com>
2023-05-30 17:48:12 +0000

    Write a class Square that defines a square by: (based on 4-square.py)

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+""" Create a square class"""
+
+
+class Square:
+    """ Define a class square """
+    def __init__(self, size=0):
+        """ Initializing a square class
+        Arguments: size=0: size of the square
+         """
+        self.__size = size
+
+    @property
+    def size(self):
+        """ Get the size of the square """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Set the size of the square """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """ Calculate the area of the square """
+        return (self.__size ** 2)
+
+    def __eql__(self, other):
+        """ Compare two squares """
+        return self.area() == other.area()
+
+    def __nt_e__(self, other):
+        """ Compare != two squares """
+        return self.area() != other.area()
+
+    def __les__(self, other):
+        """ Comparing < two squares """
+        return self.area() < other.area()
+
+    def __les_e__(self, other):
+        """ Compare <= two squares """
+        return self.area() <= other.area()
+
+    def __grt__(self, other):
+        """ Compare > two squares """
+        return self.area() > other.area()
+
+    def __grt_e__(self, other):
+        """ Compare >= two squares """
+        return self.area() >= other.area()
