f4492330440c1ee26ee33539b87f5ba42d08db5f
roochieng <49204011+roochieng@users.noreply.github.com>
2023-06-05 12:02:32 +0000

    Write a function that adds 2 integers.

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Function that adds two integers"""
+
+def add_integer(a, b=98):
+    """Convert numbers into ints and add them"""
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    
+    return (int(a) + int(b))
