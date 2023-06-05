e5d3f263d7e113e160390660ca162d5280bb996c
roochieng <49204011+roochieng@users.noreply.github.com>
2023-06-05 16:32:40 +0000

    Write a class Rectangle that defines a rectangle by: (based on 7-rectangle.py)

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -6,6 +6,7 @@ class Rectangle:
     """string representation of a rectangle"""
 
     number_of_instances = 0
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         self.__width = width
@@ -52,8 +53,8 @@ class Rectangle:
         if self.__height == 0 or self.__width == 0:
             return total
         for i in range(self.__height):
-            total += ("#" * self.__width)
-            if i is not self.__height - 1:
+            total += (str(self.print_symbol) * self.__width)
+            if i != self.__height - 1:
                 total += "\n"
         return total
 
@@ -62,4 +63,14 @@ class Rectangle:
 
     def __del__(self):
         print("Bye rectangle...")
-        Rectangle.number_of_instances -= 1
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if type(rect_1) is not Rectangle:
+            raise TypeError('rect_1 must be an instance of Rectangle')
+
+        elif type(rect_2) is not Rectangle:
+            raise TypeError('rect_2 must be an instance of Rectangle')
+
+        if rect_1.area() >= rect_2.area():
+            return rect_1
