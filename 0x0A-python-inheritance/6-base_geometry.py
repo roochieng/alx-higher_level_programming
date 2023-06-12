26cec4f129342fb694a7897842cce5f3b4402cde
roochieng <49204011+roochieng@users.noreply.github.com>
2023-06-12 16:52:10 +0000

    Write a class BaseGeometry (based on 5-base_geometry.py).
    Public instance method: def area(self): that raises an Exception with the message area() is not implemented

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Creating BaseGeometry class """
+
+
+class BaseGeometry:
+    """
+    BaseGeometry class
+    """
+    def area(self):
+        """
+        area method
+        """
+        raise Exception("area() is not implemented")
