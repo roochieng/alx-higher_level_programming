57eb1af2cf1d8bdca88da6963c58d4fe57adfe2c
roochieng <49204011+roochieng@users.noreply.github.com>
2023-07-18 20:16:29 +0000

    Pushing the mandatory tasks

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- Creates the database `hbtn_0c_0` in MySQL Server.
+--      If the database already exists, the script shouldn't fail
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,7 @@
+-- List all records of the table `second_table`
+-- of the database `hbtn_0c_0` in MySQL Server.
+
+SELECT score, name
+FROM second_table
+WHERE `name` IS NOT NULL
+ORDER BY score DESC;
