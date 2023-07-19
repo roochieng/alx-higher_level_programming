c4fb2c5f8ae3a921d582e5bc293adddaafab1b7a
roochieng <49204011+roochieng@users.noreply.github.com>
2023-07-19 18:45:54 +0000

    Update first task

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
 -- Lists all privileges of the MySQL Users `user_0d_1`
 -- and `user_0d_2` in `localhost`.
-SELECT * 
-FROM mysql.user 
-WHERE User IN ('user_0d_1', 'user_0d_2');
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
