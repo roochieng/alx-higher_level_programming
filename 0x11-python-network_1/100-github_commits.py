3bc2ce50feceba691fce368b26425708d1d25015
roochieng <49204011+roochieng@users.noreply.github.com>
2023-09-05 19:43:54 +0000

    commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """
 Python script that takes 2 arguments in order to solve this challenge.
 """
@@ -8,11 +9,12 @@ import requests
 if __name__ == "__main__":
     url = f"https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits"
 
-    m = requests.get(url)
-    commits = m.json()
+    message = requests.get(url)
+    commits = message.json()
     print(commits)
     try:
         for i in range(10):
-            print(f"{commits[i].get('sha')}: {commits[i].get('commit').get('author').get('name')}")
+            print(f"{commits[i].get('sha')}:
+                  {commits[i].get('commit').get('author').get('name')}")
     except IndexError:
         pass
