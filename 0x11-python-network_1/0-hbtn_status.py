cd617f9c6717ecea436d02731ce76f8c0e5cdc07
roochieng <49204011+roochieng@users.noreply.github.com>
2023-09-05 15:42:53 +0000

    Write a Python script that fetches

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """Python script fetches https://intranet.hbtn.io/status."""
 if __name__ == "__main__":
     import urllib.request
@@ -6,6 +7,6 @@ if __name__ == "__main__":
         html = response.read()
 
         print('Body response:')
-        print(f'\t- type: {type(html}')
+        print(f'\t- type: {type(html)}')
         print(f'\t- content: {html}')
         print(f'\t- utf8 content: {html.decode("utf-8", "replace")}')
