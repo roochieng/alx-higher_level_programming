1c8a51f0aebb2288eec9dadaf7b026964296f892
roochieng <49204011+roochieng@users.noreply.github.com>
2023-09-05 19:31:58 +0000

    commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,13 +7,13 @@ import requests
 
 
 if __name__ == "__main__":
-    url = f"https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits"
+    url = f"https://api.github.com/repos/sys.argv[2]/sys.argv[1]/commits"
 
     m = requests.get(url)
     commits = m.json()
+    print(commits)
     try:
         for i in range(10):
-            print(f"{commits[i].get('sha')}:
-                  {commits[i].get('commit').get('author').get('name')}")
+            print(f"{commits[i].get('sha')}: {commits[i].get('commit').get('author').get('name')}")
     except IndexError:
         pass
