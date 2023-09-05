cbd07fd2f7b0007373075bc3513d3ea437d56cfd
roochieng <49204011+roochieng@users.noreply.github.com>
2023-09-05 19:33:39 +0000

    commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,19 +1,19 @@
-#!/usr/bin/python3
-"""
-Python script that takes 2 arguments in order to solve this challenge.
+"""Python script that takes 2 arguments in order to solve this challenge.
 """
 import sys
 import requests
 
 
 if __name__ == "__main__":
-    url = f"https://api.github.com/repos/sys.argv[2]/sys.argv[1]/commits"
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
 
     m = requests.get(url)
     commits = m.json()
-    print(commits)
     try:
         for i in range(10):
-            print(f"{commits[i].get('sha')}: {commits[i].get('commit').get('author').get('name')}")
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
     except IndexError:
         pass
