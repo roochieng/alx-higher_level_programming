86c5b652520f2c918bd66b262c110fabba961406
roochieng <49204011+roochieng@users.noreply.github.com>
2023-09-05 18:10:02 +0000

    sends a POST request to the passed URL

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Write a Python script that takes in a URL and\
+an email address, sends a POST request to the\
+ passed URL with the email as a parameter,\
+ and finally displays the body of the response.
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    payload = {'email': sys.argv[2]}
+    r = requests.post(url, data=payload)
+    print(r.text)
