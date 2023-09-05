ccdae9c8871f1443b4c9980ea6212d6df6666aea
roochieng <49204011+roochieng@users.noreply.github.com>
2023-09-05 15:46:30 +0000

    Write a Python script that fetches

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,9 +3,9 @@
 if __name__ == "__main__":
     import urllib.request
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')\
+            as response:
         html = response.read()
-
         print('Body response:')
         print(f'\t- type: {type(html)}')
         print(f'\t- content: {html}')
